82b00f786610bf51d8e35c64856e182e12e87e79
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-15 13:27:25 +0530

    add event listners to playPause btn with keydown too

--- Pomodoro-Timer/scripts/musicPlayer.js
+++ Pomodoro-Timer/scripts/musicPlayer.js
@@ -14,8 +14,12 @@ document.querySelector("body").addEventListener("keydown", (event) => {
 
 function playPauseSong() {
   if (song.paused) {
+    playPause.classList.toggle("ri-pause-fill");
+    playPause.classList.toggle("ri-play-fill");
     song.play();
   } else {
+    playPause.classList.toggle("ri-pause-fill");
+    playPause.classList.toggle("ri-play-fill");
     song.pause();
   }
 }
