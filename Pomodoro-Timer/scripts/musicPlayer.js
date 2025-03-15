92a4520d01f3cbd5628e69c8ea21a003b3c97db9
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-15 13:51:31 +0530

    add song info - title and duration

--- Pomodoro-Timer/scripts/musicPlayer.js
+++ Pomodoro-Timer/scripts/musicPlayer.js
@@ -1,6 +1,9 @@
 const song = document.getElementById("song");
 
 const playPause = document.getElementById("playPause");
+const currentTimeEl = document.getElementById("currentTime");
+const durationEl = document.getElementById("duration");
+const songTitleEl = document.getElementById("song-title");
 
 playPause.addEventListener("click", () => {
   playPauseSong();
@@ -10,6 +13,17 @@ document.querySelector("body").addEventListener("keydown", (event) => {
   if (event.key === " ") {
     playPauseSong();
   }
+});
+
+// song title
+songTitleEl.innerText = getSongTitle();
+
+song.addEventListener("loadedmetadata", () => {
+  durationEl.innerText = formatTime(song.duration);
+});
+
+song.addEventListener("timeupdate", () => {
+  currentTimeEl.innerText = formatTime(song.currentTime);
 });
 
 function playPauseSong() {
@@ -22,4 +36,20 @@ function playPauseSong() {
     playPause.classList.toggle("ri-play-fill");
     song.pause();
   }
+}
+
+console.log(song.getElementsByTagName("source")[0].src);
+
+function formatTime(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+function getSongTitle() {
+  const songLink = song.getElementsByTagName("source")[0].src;
+  const songName = songLink
+    .substring(songLink.lastIndexOf("/") + 1, songLink.lastIndexOf("."))
+    .replace(/%20/g, " ");
+  return songName;
 }
