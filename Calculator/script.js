a9e60e18111e9ed9c6feb8054744154f18ec1ceb
kamal-stark-dev <kamalveer356@gmail.com>
2025-03-28 09:17:51 +0530

    add calculator design

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const power = (a, b) => a ** b;
+
+// will be used to update the display
+let num1, operation, num2;
+
+function operate(a, op, b) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return console.error("Invalid Operation");
+  }
+}
