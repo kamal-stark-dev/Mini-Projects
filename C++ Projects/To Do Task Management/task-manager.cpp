268387f9e77290a3b2ffe383699ef56e96ca313c
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-16 13:24:47 +0530

    add showTasks and markComplete implementation

--- C++ Projects/To Do Task Management/task-manager.cpp
+++ C++ Projects/To Do Task Management/task-manager.cpp
@@ -3,16 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 #define TasksFile "tasks.txt"
 #define CompletedTasksFile "completed.txt"
 
-// Symbols -> ✓ | ✗ | ☐ | ☑
-
 // function declarations
 void addTask();
-void viewTasks();
+void showTasks(bool showCompleted = true);
 void markComplete();
 
 int main(void) {
@@ -27,10 +26,10 @@ int main(void) {
                 addTask();
                 break;
             case 2:
-                // viewTasks();
+                showTasks();
                 break;
             case 3:
-                // markComplete();
+                markComplete();
                 break;
             case 4:
                 cout << "\n***** ~ Ara Ara Sayonara ~ *****\n";
@@ -55,6 +54,110 @@ void addTask() {
     cin.ignore(); // clear leftover newline from previous input
     getline(cin, newTask); // used getline to include spaces as well
 
-    myFile << "0 | " << newTask << "\n";
+    myFile << newTask << "\n";
     myFile.close();
+}
+
+void showTasks(bool showCompleted) {
+    ifstream myFile(TasksFile);
+    if (!myFile) {
+        cerr << "Unable to open tasks file.\n";
+        return;
+    }
+
+    cout << "\nTo Do Tasks:\n";
+
+    string buffer;
+    int i = 1;
+    // show to do tasks
+    while (getline(myFile, buffer)) {
+        cout << "    " << i << ". "  << buffer << "\n";
+        i++;
+    }
+    if (i == 1) { // task list is empty
+        cout << "    ***** The task list is empty. *****\n";
+    }
+    myFile.close();
+
+    if (!showCompleted) return;
+
+    cout << "\nCompleted Tasks:\n";
+
+    ifstream completedTasks(CompletedTasksFile);
+    if (!completedTasks) {
+        cerr << "Error in opening completed tasks file.\n";
+        return;
+    }
+
+    i = 1;
+    // show completed tasks
+    while (getline(completedTasks, buffer)) {
+        cout << "    " << i << ". " << buffer << "\n";
+        i++;
+    }
+    if (i == 1) {
+        cout << "    ***** The task list is empty. *****\n";
+    }
+    completedTasks.close();
+}
+
+void markComplete() {
+    int taskNumber;
+    showTasks(false);
+    cout << "\nSelect the task number of the task which you want to mark as complete: ";
+    cin >> taskNumber;
+
+    if (taskNumber <= 0) { // 0 or negative index is invalid
+        cout << "Invalid task number provided.\n";
+        return;
+    }
+
+    ifstream inFile(TasksFile);
+    if (!inFile) {
+        cerr << "Error in opening tasks file.\n";
+        return;
+    }
+
+    int lineNumber = 0;
+    vector<string> lines;
+    string line, completedTask;
+    bool foundCompletedTask = false;
+
+    // store completedTask and all lines except completedTask
+    while (getline(inFile, line)) {
+        lineNumber++;
+        if (lineNumber == taskNumber) {
+            completedTask = line;
+            foundCompletedTask = true;
+            continue;
+        }
+        lines.push_back(line);
+    }
+    inFile.close();
+
+    if (!foundCompletedTask) { // invalid taks number was provided
+        cout << "Invalid task number provided.\n";
+        return;
+    }
+
+    // rewrite new task list (completed task removed)
+    ofstream outFile(TasksFile);
+    if (!outFile) {
+        cerr << "Error in removing task from tasks file.\n";
+        return;
+    }
+    for (string& line: lines) {
+        outFile << line << "\n";
+    }
+    outFile.close();
+
+    // adding completed task to completedTasks file
+    ofstream completedFile(CompletedTasksFile, ios::app);
+    if (!completedFile) {
+        cerr << "Error in adding task to completed file.\n";
+        return;
+    }
+
+    completedFile << completedTask << "\n";
+    completedFile.close();
 }
