61b33af82c41ee421a493ec212b11a3f7fec1983
kamal-stark-dev <kamalveer356@gmail.com>
2025-08-16 12:27:29 +0530

    add addTask implementation

--- C++ Projects/To Do Task Management/task-manager.cpp
+++ C++ Projects/To Do Task Management/task-manager.cpp
@@ -2,21 +2,32 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+#define TasksFile "tasks.txt"
+#define CompletedTasksFile "completed.txt"
+
+// Symbols -> ✓ | ✗ | ☐ | ☑
+
+// function declarations
+void addTask();
+void viewTasks();
+void markComplete();
 
 int main(void) {
     // Menu Loop - Lets the user choose an action until they quit.
     int choice;
     do {
-        cout << "\nTo Do List - Menu\n\t1. View tasks.\t2. Add task.\t3. Mark as complete.\t4. Exit.\n>> Select an operation: ";
+        cout << "\nTo Do List - Menu\n\t1. Add task.\t2. View tasks.\t3. Mark as complete.\t4. Exit.\n>> Select an operation: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                // showTasks();
+                addTask();
                 break;
             case 2:
-                // addTask();
+                // viewTasks();
                 break;
             case 3:
                 // markComplete();
@@ -30,4 +41,20 @@ int main(void) {
     } while (choice != 4);
 
     return 0;
+}
+
+void addTask() {
+    ofstream myFile(TasksFile, ios::app);
+    if (!myFile.is_open()) {
+        cerr << "There was an error in opening the file.\n";
+        return;
+    }
+
+    string newTask;
+    cout << "Enter new task: ";
+    cin.ignore(); // clear leftover newline from previous input
+    getline(cin, newTask); // used getline to include spaces as well
+
+    myFile << "0 | " << newTask << "\n";
+    myFile.close();
 }
